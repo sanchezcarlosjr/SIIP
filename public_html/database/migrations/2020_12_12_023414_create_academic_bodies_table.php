5f86d73d12efc54d40b66d2e3f07ea8cc1e64e0e
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-01-18 13:36:42 -0800

    fix(git): error: inflate: data stream error (incorrect data check)

--- public_html/database/migrations/2020_12_12_023414_create_academic_bodies_table.php
+++ public_html/database/migrations/2020_12_12_023414_create_academic_bodies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAcademicAodiesTable extends Migration
+class CreateAcademicBodiesTable extends Migration
 {
     /**
      * Run the migrations.
