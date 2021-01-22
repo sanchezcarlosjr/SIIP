dc56d49875384227ed4f6da0f1dd9b60d224d565
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-01-22 10:55:14 -0800

    feat(navbar): better design

--- public_html/resources/js/entry.component.vue
+++ public_html/resources/js/entry.component.vue
@@ -18,8 +18,8 @@
             </div>
             <nav class="navbar navbar-header navbar-expand-lg" data-background-color="green">
                 <div class="container-fluid">
-                    <div id="search-nav">
-                        <span style="color: #ffffff;max-width: 500px; ">SISTEMA INSTITUCIONAL DE INDICADORES DE INVESTIGACION Y POSGRADO</span>
+                    <div>
+                        <span style="color: #ffffff;max-width: 500px; ">Sistema institucional de indicadores de investigación y posgrado</span>
                     </div>
                 </div>
             </nav>
