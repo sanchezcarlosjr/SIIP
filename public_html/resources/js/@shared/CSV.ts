bd93bf27639f915494fc10e2df5064b8ad642938
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-01-22 20:16:43 -0800

    fix(download-buttons): remove extra

--- public_html/resources/js/@shared/CSV.ts
+++ public_html/resources/js/@shared/CSV.ts
@@ -9,26 +9,12 @@ export class CSV {
         var downloadLink;
         var BOM = "\uFEFF";
         var csvData = BOM + this.csv;
-
-        // CSV file
         csvFile = new Blob([csvData], {type: "text/csv"});
-
-        // Download link
         downloadLink = document.createElement("a");
-
-        // File name
         downloadLink.download = this.filename;
-
-        // Create a link to the file
         downloadLink.href = window.URL.createObjectURL(csvFile);
-
-        // Hide download link
         downloadLink.style.display = "none";
-
-        // Add the link to DOM
         document.body.appendChild(downloadLink);
-
-        // Click download link
         downloadLink.click();
     }
     load() {
