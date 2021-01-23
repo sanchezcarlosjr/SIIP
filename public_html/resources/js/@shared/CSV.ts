04eb65b247c1f33b8bd9a2f3b9cb458d46e4241b
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-01-22 20:20:39 -0800

    fix(download-buttons): remove extra

--- public_html/resources/js/@shared/CSV.ts
+++ public_html/resources/js/@shared/CSV.ts
@@ -5,10 +5,10 @@ export class CSV {
     constructor(private filename: string) {
     }
     download() {
-        var csvFile;
-        var downloadLink;
-        var BOM = "\uFEFF";
-        var csvData = BOM + this.csv;
+        let csvFile;
+        let downloadLink: any;
+        const BOM = "\uFEFF";
+        const csvData = BOM + this.csv;
         csvFile = new Blob([csvData], {type: "text/csv"});
         downloadLink = document.createElement("a");
         downloadLink.download = this.filename;
@@ -18,12 +18,12 @@ export class CSV {
         downloadLink.click();
     }
     load() {
-        var csv = [];
-        var rows = document.querySelectorAll("table tr");
-        for (var i = 0; i < rows.length; i++) {
-            var row = [], cols = rows[i].querySelectorAll("td, th");
+        const csv = [];
+        const rows = document.querySelectorAll("table tr");
+        for (let i = 0; i < rows.length; i++) {
+            const row = [], cols = rows[i].querySelectorAll("td, th");
 
-            for (var j = 0; j < cols.length-1; j++)
+            for (let j = 0; j < cols.length-1; j++)
                 { // @ts-ignore
                     row.push(cols[j].innerText.replace(/\n\(.*\)/, ''));
                 }
