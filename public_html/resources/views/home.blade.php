7cbb91c8847a70dce1cf1ec3d6b6545707f4d779
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-01-29 16:21:35 -0800

    fix(path): from relative path to asset path

--- public_html/resources/views/home.blade.php
+++ public_html/resources/views/home.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <title>SIIIP</title>
-    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport'/>
     <link rel="icon" href="/img/logo.png" type="image/x-icon"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <!--
@@ -11,17 +11,17 @@
        © 2021 Universidad Autónoma de Baja California
        © 2021 sanchezcarlosjr.com <hello@sanchezcarlosjr.com>
     -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/atlantis.css">
-    <link rel="stylesheet" href="/css/demo.css">
-	<link href="{{asset('css/app.css')}}" rel="stylesheet">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/atlantis.css')}}">
+    <link rel="stylesheet" href="{{asset('css/demo.css')}}">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-    <div id="app" class="content">
-        <entry-component></entry-component>
-    </div>
-  <script src="{{asset('js/app.js')}}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+<div id="app" class="content">
+    <entry-component></entry-component>
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
 </body>
 </html>
