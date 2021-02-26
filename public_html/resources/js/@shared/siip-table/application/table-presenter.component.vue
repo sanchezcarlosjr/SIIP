ca1d48c4baa07e1389d6545c4512c0750f0e7a47
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-26 11:32:56 -0800

    refactor(siip-table): change to table-presenter

--- public_html/resources/js/@shared/siip-table/application/table-presenter.component.vue
+++ public_html/resources/js/@shared/siip-table/application/table-presenter.component.vue
@@ -1,47 +1,69 @@
 <template>
-    <div>
-        <b-table
-            id="main-table"
-            ref="table"
-            :busy="busy"
-            :current-page="currentPage"
-            :fields="fields"
-            :filter="criteria"
-            :filter-function="search"
-            :items="items"
-            :per-page="perPage"
-            :sort-by.sync="sortBy"
-            :sort-desc.sync="sortDesc"
-            :sort-direction="sortDirection"
-            empty-filtered-text="Sin resultados"
-            emptyText="Sin elementos"
-            head-variant="light"
-            hover
-            responsive="sm"
-            show-empty
-            small
-            stacked="md"
-            sticky-header
-            striped
-            @row-clicked="rowClicked"
-            @row-contextmenu="rowContextMenu"
-        >
-            <template #table-busy>
-                <b-skeleton-table
-                    :columns="fields.length"
-                    :rows="3"
-                ></b-skeleton-table>
-            </template>
-        </b-table>
-        <context-menu
-            :ref="'contextMenu'"
-            :elementId="'myFirstMenu'"
-            :links="rowContextLinks"
-            :options="rowContextOptions"
-            @option-clicked="optionClicked"
-        >
-        </context-menu>
-    </div>
+    <b-container class="p-0 m-0" fluid>
+        <div class="b-table-sticky-header">
+            <b-table
+                id="main-table"
+                ref="table"
+                :busy="busy"
+                :current-page="currentPage"
+                :fields="fields"
+                :filter="criteria"
+                :filter-function="search"
+                :items="items"
+                :per-page="perPage"
+                :sort-by.sync="sortBy"
+                :sort-desc.sync="sortDesc"
+                :sort-direction="sortDirection"
+                empty-filtered-text="Sin resultados"
+                emptyText="Sin elementos"
+                head-variant="light"
+                hover
+                responsive="sm"
+                show-empty
+                small
+                stacked="md"
+                sticky-header
+                striped
+                @row-clicked="rowClicked"
+                @row-contextmenu="rowContextMenu"
+            >
+                <template #table-busy>
+                    <b-skeleton-table
+                        :columns="fields.length"
+                        :rows="3"
+                    ></b-skeleton-table>
+                </template>
+            </b-table>
+            <context-menu
+                :ref="'contextMenu'"
+                :elementId="'myFirstMenu'"
+                :links="rowContextLinks"
+                :options="rowContextOptions"
+                @option-clicked="optionClicked"
+            >
+            </context-menu>
+        </div>
+        <b-container class="bv-example-row">
+            <b-row align-h="between">
+                <b-col cols="4">
+                    <p>
+                        <b-form-select id="perPageSelected" v-model="perPage" :options="[5, 10, 25]" class="mt-0 w-25"
+                                       size="sm"></b-form-select>
+                        resultados por página.
+                    </p>
+                </b-col>
+                <b-col cols="4">
+                    <b-pagination
+                        v-model="currentPage"
+                        :per-page="perPage"
+                        :total-rows="rows"
+                        aria-controls="main-table"
+                        class="d-flex justify-content-end"
+                    ></b-pagination>
+                </b-col>
+            </b-row>
+        </b-container>
+    </b-container>
 </template>
 
 <script>
@@ -51,16 +73,25 @@ export default {
         'busy',
         'items',
         'fields',
-        'currentPage',
         'criteria',
         'search',
-        'perPage',
         'sortBy',
         'sortDesc',
         'sortDirection',
         'rowContextOptions',
         'rowContextLinks'
     ],
+    data() {
+        return {
+            perPage: 10,
+            currentPage: 1
+        }
+    },
+    computed: {
+        rows() {
+            return this.items.length;
+        }
+    },
     methods: {
         rowClicked(item, index, event) {
             this.$emit('rowClicked', item, index, event);
@@ -78,3 +109,14 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.container {
+    max-width: 100%;
+}
+
+.b-table-sticky-header {
+    overflow-y: auto;
+    max-height: 70vh;
+}
+</style>
--- public_html/resources/js/@shared/siip-table/index.vue
+++ public_html/resources/js/@shared/siip-table/index.vue
@@ -83,95 +83,70 @@
                 </b-row>
             </b-container>
         </div>
-        <b-container fluid class="p-0 m-0">
-            <div class="b-table-sticky-header">
-                <table-presenter
-                    :busy="$apollo.loading"
-                    :items="items"
-                    :fields="tableFields"
-                    :current-page="currentPage"
-                    :filter="criteria"
-                    :filter-function="search"
-                    :per-page="perPage"
-                    :sort-by.sync="sortBy"
-                    :sort-desc.sync="sortDesc"
-                    :sort-direction="sortDirection"
-                    :rowContextLinks="links"
-                    :rowContextOptions="options"
-                    @optionClicked="optionClicked"
-                    @rowClicked="edit"
-                >
-                </table-presenter>
-            </div>
-            <b-container class="bv-example-row">
-                <b-row align-h="between">
-                    <b-col cols="4">
-                        <p>
-                            <b-form-select id="perPageSelected" v-model="perPage" :options="[5, 10, 25]" size="sm" class="mt-0 w-25"></b-form-select>
-                            resultados por página.
-                        </p>
-                    </b-col>
-                    <b-col cols="4">
-                        <b-pagination
-                            v-model="currentPage"
-                            :total-rows="rows"
-                            class="d-flex justify-content-end"
-                            :per-page="perPage"
-                            aria-controls="main-table"
-                        ></b-pagination>
-                    </b-col>
-                </b-row>
-            </b-container>
-            <create-modal-component
-                :form-options="formOptions"
-                :model="infoModal.model"
-                :ok-disabled="okDisabled"
-                :schema="schema"
-                :title="infoModal.title"
-                @ok="execute"
-                @onValidated="onValidated"
-                @reset="resetModal"
-            ></create-modal-component>
-            <b-modal
-                :id="'editCollapse'+infoModal.resource"
-                :hide-footer="true"
-                :title="infoModal.title"
-                cancel-title="Cancelar"
-                ok-title="Aceptar"
-                scrollable
-                size="xl"
-                @hide="hideModal"
-            >
-                <b-nav tabs>
-                    <b-nav-item
-                        v-for="(value, key) in links" :key="key"
-                        :to="value.link.replace('*', infoModal.itemId)"
-                        active-class="active">
-                        <i class="fas" style="font-size:20px"
-                           v-bind:class="'fa-'+key"></i>
-                        {{ value.tooltip }}
-                    </b-nav-item>
-                </b-nav>
-                <router-view :key="$route.path"></router-view>
-            </b-modal>
-            <edit-modal-component
-                :title="infoModal.title"
-                :model="infoModal.model"
-                :schema="schema"
-                @reset="resetModal"
-                @ok="execute"
-            ></edit-modal-component>
-            <remove-modal-component
-                :resource="infoModal.resource"
-                :title="infoModal.title"
-                @ok="execute"
-                @reset="resetModal"
-            ></remove-modal-component>
-            <b-modal id="archive" :title="infoModal.title" cancel-title="Cancelar" ok-title="Si, deseo archivarlo"
-                     scrollable @hide="resetModal" @ok="execute">
-                <p>¿Realmente desea archivar a este {{ infoModal.resource }}?</p>
-            </b-modal>
-        </b-container>
+        <table-presenter
+            :busy="$apollo.loading"
+            :fields="tableFields"
+            :filter="criteria"
+            :filter-function="search"
+            :items="items"
+            :rowContextLinks="links"
+            :rowContextOptions="options"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :sort-direction="sortDirection"
+            @optionClicked="optionClicked"
+            @rowClicked="edit"
+        >
+        </table-presenter>
+        <create-modal-component
+            :form-options="formOptions"
+            :model="infoModal.model"
+            :ok-disabled="okDisabled"
+            :schema="schema"
+            :title="infoModal.title"
+            @ok="execute"
+            @onValidated="onValidated"
+            @reset="resetModal"
+        ></create-modal-component>
+        <b-modal
+            :id="'editCollapse'+infoModal.resource"
+            :hide-footer="true"
+            :title="infoModal.title"
+            cancel-title="Cancelar"
+            ok-title="Aceptar"
+            scrollable
+            size="xl"
+            @hide="hideModal"
+        >
+            <b-nav tabs>
+                <b-nav-item
+                    v-for="(value, key) in links" :key="key"
+                    :to="value.link.replace('*', infoModal.itemId)"
+                    active-class="active">
+                    <i class="fas" style="font-size:20px"
+                       v-bind:class="'fa-'+key"></i>
+                    {{ value.tooltip }}
+                </b-nav-item>
+            </b-nav>
+            <router-view :key="$route.path"></router-view>
+        </b-modal>
+        <edit-modal-component
+            :model="infoModal.model"
+            :schema="schema"
+            :title="infoModal.title"
+            @ok="execute"
+            @reset="resetModal"
+        ></edit-modal-component>
+        <remove-modal-component
+            :resource="infoModal.resource"
+            :title="infoModal.title"
+            @ok="execute"
+            @reset="resetModal"
+        ></remove-modal-component>
+        <b-modal id="archive" :title="infoModal.title" cancel-title="Cancelar" ok-title="Si, deseo archivarlo"
+                 scrollable @hide="resetModal" @ok="execute">
+            <p>¿Realmente desea archivar a este {{ infoModal.resource }}?</p>
+        </b-modal>
     </div>
 </template>
 
