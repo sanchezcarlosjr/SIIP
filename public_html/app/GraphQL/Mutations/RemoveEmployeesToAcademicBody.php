71522bbf22fb947e91c459311fae914b94941a34
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-03-05 16:16:27 -0800

    feat(networks): show networks

--- public_html/app/GraphQL/Mutations/RemoveEmployeesToAcademicBody.php
+++ public_html/app/GraphQL/Mutations/RemoveEmployeesToAcademicBody.php
@@ -8,12 +8,12 @@ class RemoveEmployeesToAcademicBody
 {
     public function __invoke($_, array $args)
     {
-        return Employee::find($args['employee_id'])
+        return Employee::find($args['employees_id'])
             ->academic_bodies_lgacs()
             ->get()
             ->where('academic_body_id', $args['academic_body_id'])
             ->first()
-            ->detach($args['employee_id'])
+            ->detach($args['employees_id'])
             ->employees()
             ->detach();
     }
