5dd013125236bdf47ec1460530963b9f9ba2d5f7
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-04-09 13:02:14 -0700

    refactor(academic_body): custom name_like

--- public_html/app/GraphQL/Queries/Employee.php
+++ public_html/app/GraphQL/Queries/Employee.php
@@ -11,17 +11,26 @@ class Employee
         if (count($filter) == 0) {
             return $builder;
         }
-        $nameOrId = $filter[0];
+        return $this->name_like($builder, $filter[0]);
+    }
+
+    public function is_lgac_member(Builder $builder, bool $m): Builder
+    {
+        return $builder->has('academic_bodies_lgacs', ($m ? '>' : '<='), 0);
+    }
+
+    /**
+     * @param Builder $builder
+     * @param String $nameOrId
+     * @return Builder
+     */
+    private function name_like(Builder $builder, string $nameOrId): Builder
+    {
         return $builder->where(function ($query) use ($nameOrId) {
             $query->where("nombre", "ILIKE", "%" . $nameOrId . "%")
                 ->orWhere("amaterno", "ILIKE", "%" . $nameOrId . "%")
                 ->orWhere("apaterno", "ILIKE", "%" . $nameOrId . "%")
                 ->orWhere("nempleado", "ILIKE", "%" . $nameOrId . "%");
         });
-    }
-
-    public function is_lgac_member(Builder $builder, bool $m): Builder
-    {
-        return $builder->has('academic_bodies_lgacs', ($m ? '>' : '<='), 0);
     }
 }
