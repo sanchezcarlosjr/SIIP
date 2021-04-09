8c652ffad29c6d5c06f22c5be51b3ff0c47ef5db
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-04-09 16:02:23 -0700

    feat(academic_body collaborator): search collaborator

--- public_html/resources/js/academic-bodies/collaborators-by-academic-body/collaborators.page.ts
+++ public_html/resources/js/academic-bodies/collaborators-by-academic-body/collaborators.page.ts
@@ -1,29 +1,24 @@
 import Vue from "vue"
 import Component from "vue-class-component"
 import {GraphqlSubResourceFinderRepository} from "../../@shared/infraestructure/communication/graphql/graphql-sub-resource-finder-repository";
+import {GraphqlResourceRepository} from "../../@shared/infraestructure/communication/graphql/graphql-resource-repository";
 
 @Component
 export default class CollaboratorsPage extends Vue {
     apiResource = GraphqlSubResourceFinderRepository.createDefaultFinder('academic_body', 'collaborators');
     spanishResourceName = 'Colaboradores'
-    toolbar = new Set(['archive', 'add', 'edit', 'remove']);
+    toolbar = new Set(['archive', 'add', 'edit']);
     fields = [
         {key: 'name', label: 'Nombre', sortable: true},
-        {key: 'description', label: 'Descripción', sortable: true}
     ];
     schema = {
         fields: [
             {
-                type: 'input',
-                inputType: 'text',
-                label: 'Nombre del Colaborador',
-                model: 'name'
-            },
-            {
-                type: 'input',
-                inputType: 'text',
-                label: 'Descripción',
-                model: 'description'
+                type: 'graphql-select-id',
+                label: 'Colaborador',
+                model: "employees_id",
+                query: new GraphqlResourceRepository(`employees(free: ${this.$route.params.id}, filter: $filter)`),
+                textKey: 'name'
             }
         ]
     };
