1dcea5b4ccd1dfbd30ca68129bc6de95e3e31b57
cest <hello@sanchezcarlosjr.com>
2021-04-23 08:47:03 -0700

    feat(graphql): match columns and load it

--- public_html/resources/js/@shared/infraestructure/communication/graphql/graphql.ts
+++ public_html/resources/js/@shared/infraestructure/communication/graphql/graphql.ts
@@ -12,18 +12,21 @@ export class ApolloSiipTableRepository extends ApolloDefaultRepository {
     }
 
     mapDataToUpdate(data: any, component: any) {
-        const columns: any = {};
-        component.fields.filter((field: any) => field.column).forEach((field: any) => {
-                data.forEach((item: any) =>
-                    item[field.column].forEach((column: any) => {
-                        columns[column.id] = column.name;
-                        item._cellVariants = { [`${column.id}`]: 'success' }
-                    })
-                );
+        const fields = component.fields.filter((field: any) => field.column);
+        if (fields.length > 0) {
+            const columns: any = {};
+            fields.forEach((field: any) => {
+                    data.forEach((item: any) =>
+                        item[field.column].forEach((column: any) => {
+                            columns[column.id] = column.name;
+                            item[column.id] = true;
+                        })
+                    );
+                }
+            );
+            for (const id in columns) {
+                component.tableFields.push({key: id, label: columns[id]});
             }
-        );
-        for (const id in columns) {
-            component.tableFields.push({key: id, label: columns[id]});
         }
         return data;
     }
