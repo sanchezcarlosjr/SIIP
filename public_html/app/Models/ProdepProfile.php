637aefc2af83683b2632c335a9a2fe511a358ad8
David <dmgf2008@hotmail.com>
2021-05-05 14:16:53 -0700

    Stacking Scopes on Prodep

--- public_html/app/Models/ProdepProfile.php
+++ public_html/app/Models/ProdepProfile.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 
 class ProdepProfile extends Model
@@ -12,7 +13,12 @@ class ProdepProfile extends Model
     use HasFactory;
     use ActiveEmployee;
 
-    protected $fillable = ["start_date", "finish_date", "employee_id", "prodep_area_id"];
+    protected $fillable = [
+      "start_date",
+      "years_to_finish",
+      "employee_id",
+      "prodep_area_id"
+    ];
     protected $appends = [
       "is_active"
     ];
@@ -31,4 +37,86 @@ class ProdepProfile extends Model
         return $this->belongsTo(Employee::class, "employee_id");
     }
 
+    public function getYearsToFinishAttribute() {
+      if(isset($this->finish_date)) {
+        return Carbon::parse($this->finish_date)->diffInYears(Carbon::parse($this->start_date));
+      } else {
+        return -1;
+      }
+    }
+
+    public function setYearsToFinishAttribute($years) {
+      $this->attributes["finish_date"] = Carbon::parse($this->start_date)->addYears($years);
+    }
+
+    public function scopeMostRecent($query) {
+      return $query->orderBy("start_date", "DESC");
+    }
+
+    public function scopeAcademicUnit($query, $name) {
+      return $query
+        ->joinSub(function($query) use ($name) {
+          $query
+            ->select("*")
+            ->fromSub(function($query) use ($name) {
+              $query
+                ->select("prodep_profiles.*", "unidades.unidad as unidad")
+                ->from("prodep_profiles")
+                ->join("empleados", function($join) {
+                  $join->on("prodep_profiles.employee_id","=","empleados.nempleado");
+                })
+                ->join("unidades", function($join) {
+                  $join->on("empleados.nunidad","=","unidades.nunidad");
+                })
+                ->where("unidad", "=", $name);
+              }, "inner_terms");
+        }, "academic_unit", function($join) {
+          $join->on("prodep_profiles.id", "=", "academic_unit.id");
+        })
+        ->select("prodep_profiles.*");
+    }
+
+    public function scopeTerms($query, $terms) {
+      if (empty($terms)) {
+        return $query;
+      }
+
+      $where = [];
+      for ($i = 0; $i < count($terms); $i++) {
+        $where[] = array(DB::raw("CONCAT_WS(nombre, apaterno, amaterno, employee_id, unidad, start_date, finish_date)"), "ILIKE", "%".$terms[$i]."%");
+      }
+
+
+      return $query
+        ->joinSub(function($query) use ($where) {
+          $query
+            ->select("*")
+            ->fromSub(function($query) {
+              $query
+                ->select(
+                  "prodep_profiles.*",
+                  "prodep_areas.name as prodep_area",
+                  "unidades.unidad",
+                  "empleados.nombre",
+                  "empleados.apaterno",
+                  "empleados.amaterno")
+                ->from("prodep_profiles")
+                ->join("prodep_areas", function($join) {
+                  $join->on("prodep_profiles.prodep_area_id","=","prodep_areas.id");
+                })
+                ->join("empleados", function($join) {
+                  $join->on("prodep_profiles.employee_id","=","empleados.nempleado");
+                })
+                ->join("unidades", function($join) {
+                  $join->on("empleados.nunidad","=","unidades.nunidad");
+                });
+            }, "inner_terms")
+            ->where(function ($query) use ($where) {
+              $query->orWhere($where);
+            });
+        }, "terms", function ($join) {
+          $join->on("prodep_profiles.id", "=", "terms.id");
+        })
+        ->select("prodep_profiles.*");
+    }
 }
