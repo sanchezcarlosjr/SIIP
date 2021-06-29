4135a873886e9c41b0fcff5640a2bbcd86f74391
Carlos Eduardo Sanchez Torres <hello@sanchezcarlosjr.com>
2021-06-29 14:55:54 -0700

    feat(academic-bodies permissions): show or hide forms by user permissions

--- public_html/resources/js/academic-bodies/academic-body-management/academic-body-management.page.ts
+++ public_html/resources/js/academic-bodies/academic-body-management/academic-body-management.page.ts
@@ -4,9 +4,10 @@ import {validator as GraphQLSelectIdValidator} from "../../@shared/application/f
 
 import {academic_bodies, des} from "../../@shared/repositories/academic_bodies/repository";
 import {prodep_areas} from "../../@shared/repositories/prodep/repository";
-import {validity, campus, grade} from "../../@shared/search-criteria/search-criteria";
+import {campus, grade, validity} from "../../@shared/search-criteria/search-criteria";
 
 import AcademicBodyStatistics from './statistics/index.vue';
+import {Permission} from "../../store/auth/permission";
 
 let schema = {
     legend: "Cuerpo Académico",
@@ -87,6 +88,10 @@ let schema = {
     ]
 };
 
+const formSchema = new Permission('/cuerpos-academicos', {
+    create: schema
+});
+
 @Component({
     components: {
         AcademicBodyStatistics
@@ -125,9 +130,7 @@ export default class AcademicBodyManagementPage extends Vue {
             tooltip: 'Redes'
         }
     };
-    formSchemas = {
-        create: schema
-    }
+    formSchemas = formSchema.hasPermissions();
     fields = [
         {key: 'name', label: 'Nombre', sortable: true, editable: true, class: 'vw-20'},
         {
