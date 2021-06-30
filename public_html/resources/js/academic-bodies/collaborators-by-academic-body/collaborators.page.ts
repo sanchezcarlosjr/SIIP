fc3dc53e92c7e76f77d77a0ce0be5180b12897b4
Carlos Eduardo Sanchez Torres <hello@sanchezcarlosjr.com>
2021-06-30 11:30:48 -0700

    fix(academic_body collaborators): create collaborators

--- public_html/resources/js/academic-bodies/collaborators-by-academic-body/collaborators.page.ts
+++ public_html/resources/js/academic-bodies/collaborators-by-academic-body/collaborators.page.ts
@@ -2,8 +2,37 @@ import {Component, Vue} from 'vue-property-decorator';
 import {collaborators} from "../../@shared/repositories/academic_bodies/collaborators/repository.ts";
 import {membersForm} from "../members/members.page";
 import {Permission} from "../../store/auth/permission";
+import {employees} from "../../@shared/repositories/employees/repository.ts";
+import {validator as GraphQLSelectIdValidator} from "../../@shared/application/form-fields/vfg-field-select-graphql-id/vfg-field-select-graphql-id"
 
-const permission = new Permission('/cuerpos-academicos/:academic_body_id/colaboradores', membersForm);
+const permission = new Permission('/cuerpos-academicos/:academic_body_id/colaboradores', {
+    create: {
+        legend: "Colaborador",
+        fields: [
+            {
+                type: 'graphql-select-id',
+                label: 'Nombre del empleado*',
+                model: "employee.name",
+                query: {
+                    resource: employees,
+                    target: "name",
+                    ref: "employee_id",
+                    scopes: [
+                        {
+                            name: "name_or_id"
+                        }
+                    ]
+                },
+                required: true,
+                hint: "Número de Empleado: ",
+                validator: GraphQLSelectIdValidator({
+                    selectValid: "Seleccione un empleado válido"
+                })
+            }
+        ]
+    },
+    read: membersForm
+});
 
 @Component
 export default class CollaboratorsPage extends Vue {
